174bb4b4fc757b8df633abbb203b5c8844046dd4
Shen Haowen <shenhaowen123@gmail.com>
2018-01-11 14:27:18 +0800

    Update Week1_1.c

--- Week1_1.c
+++ Week1_1.c
@@ -31,7 +31,7 @@ int main()
 {
 	int a, b;
 	scanf("%d", &a);
-    b = a % 10 * 100 + a / 10 % 10 * 10 + a / 100;
+    b = a%10*100 + a/10%10*10 + a/100;
     printf("%d\n", b);
     
     return 0;
